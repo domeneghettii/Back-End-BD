3299d362ccc694613df39d7251646ef733a19d9b
Luana Domeneghetti <158210063+domeneghettii@users.noreply.github.com>
2025-03-11 10:35:39 -0300

    feat: finalização.

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -5,5 +5,35 @@ const getUsers = async () => {
     return result.rows;
 };
 
+const getUserById = async (id) => {
+    const result = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+    return result.rows[0];
+};
 
-module.exports = { getUsers };
+const createUser = async (name, email) => {
+    const result = await pool.query(
+        "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *",
+        [name, email]
+    );
+    return result.rows[0];
+};
+
+const updateUser = async (id, name, email) => {
+    const result = await pool.query(
+        "UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING *",
+        [name, email, id]
+    );
+    return result.rows[0];
+};
+
+const deleteUser = async (id) => {
+    const result = await pool.query("DELETE FROM users WHERE id = $1 RETURNING *", [id]);
+
+    if (result.rowCount === 0) {
+        return { error: "Usuário não encontrado." };
+    }
+
+    return { message: "Usuário deletado com sucesso." };
+};
+
+module.exports = { getUsers, getUserById, createUser, updateUser, deleteUser };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 router.get("/users", userController.getAllUsers);
-router.get("/user:id", userController.getUser);
-router.post("/user", userController.createUser);
-router.put("/user:id", userController.updateUser);
-router.delete("/user:id", userController.deleteUser);
+router.get("/users/:id", userController.getUser);
+router.post("/users", userController.createUser);
+router.put("/users/:id", userController.updateUser);
+router.delete("/users/:id", userController.deleteUser);
 
 module.exports = router;
