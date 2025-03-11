c1cfd11da48ef1ea408356c2d9b039a7f87db2d5
Luana Domeneghetti <158210063+domeneghettii@users.noreply.github.com>
2025-03-11 09:47:29 -0300

    feat:update userModel.

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
-router.get("/user", userController.getAllUsers);
+router.get("/users", userController.getAllUsers);
 router.get("/user:id", userController.getUser);
 router.post("/user", userController.createUser);
 router.put("/user:id", userController.updateUser);
